37c334c22e18363289922e0a8910c639aa579699
Abdazimov2000 <abdazimovasadbek2000@gmail.com>
2025-02-22 11:07:41 +0500

    Create homework_4.py

---
+++ lesson-1/homework/homework_4.py
@@ -0,0 +1,10 @@
+x = int(input('Insert the first number: '))
+y = int(input('Insert the second number: '))
+summ = x + y
+product = x * y
+square1 = x**2
+square2 = y**2
+print(f"The sum of numbers is {summ}.")
+print(f"The product of numbers is {product}.")
+print(f"The square of the first number is {square1}.")
+print(f"The square of the second number is {square2}.")
