fcb8ee7cebc30b06356fbf46dd832332aabd388d
Abdazimov2000 <abdazimovasadbek2000@gmail.com>
2025-02-22 16:21:42 +0500

    Create homework_6.py

---
+++ lesson-9/homework/homework_6.py
@@ -0,0 +1,10 @@
+class Num_add():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def addition(self):
+        print(self.x + self.y)
+
+z = Num_add(7, 8)
+z.addition()
